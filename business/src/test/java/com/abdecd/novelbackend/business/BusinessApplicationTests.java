5c8ec83eadffeb85be94013a06755fd3ba913cf2
abdecd <abdecd@users.noreply.github.com>
2024-03-03 09:26:46 +0800

    init

---
+++ business/src/test/java/com/abdecd/novelbackend/business/BusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdecd.novelbackend.business;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
