a98884a9ed41e0da8f0929b78068673a5506d215
abdecd <abdecd@users.noreply.github.com>
2024-05-03 22:32:21 +0800

    refactor: 提取常量

--- common/src/main/java/com/abdecd/novelbackend/common/constant/MessageConstant.java
+++ common/src/main/java/com/abdecd/novelbackend/common/constant/MessageConstant.java
@@ -14,4 +14,11 @@ public class MessageConstant {
     public static final String FAVORITES_EXIST = "已经收藏过了";
     public static final String FAVORITES_EXCEED_LIMIT = "收藏数量超过上限";
     public static final String RATE_LIMIT = "请求过于频繁，请稍后再试";
+    public static final String IMG_FILE_EMPTY = "文件为空";
+    public static final String IMG_FILE_TYPE_ERROR = "文件类型不正确";
+    public static final String IMG_FILE_SIZE_ERROR = "文件大小超过限制";
+    public static final String IMG_FILE_UPLOAD_FAIL = "图片上传失败";
+    public static final String IMG_FILE_UPLOAD_LIMIT = "图片上传次数达到上限";
+    public static final String IMG_FILE_NOT_FOUND = "图片不存在或格式错误";
+    public static final String IMG_FILE_READ_FAIL = "图片查看失败";
 }
--- common/src/main/java/com/abdecd/novelbackend/common/constant/RedisConstant.java
+++ common/src/main/java/com/abdecd/novelbackend/common/constant/RedisConstant.java
@@ -11,6 +11,11 @@ public class RedisConstant {
     public static final String COMMENT_FOR_NOVEL_TIMESTAMP = "timestamp::commentForNovel:";
     public static final String NOVEL_DAILY_READ = "novelDailyRead:";
     public static final String LIMIT_VERIFY_EMAIL = "limitVerifyEmail:";
+    public static final int LIMIT_VERIFY_EMAIL_RESET_TIME = 60;
     public static final String LIMIT_UPLOAD_IMG = "limitUploadImg:";
+    public static final int LIMIT_UPLOAD_IMG_CNT = 300;
+    public static final int LIMIT_UPLOAD_IMG_RESET_TIME = 1; // day
     public static final String LIMIT_IP_RATE = "limitIpRate:";
+    public static final int LIMIT_IP_RATE_CNT = 200;
+    public static final int LIMIT_IP_RATE_RESET_TIME = 3;
 }
